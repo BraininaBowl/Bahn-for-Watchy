6a9c7d207d853cfb243bfb3e6a449e8c582d38b3
Frans van Bellen <fransvb@gmail.com>
2021-01-26 22:20:11 +0100

    Update Bahn.ino

--- Bahn.ino
+++ Bahn.ino
@@ -1,6 +1,6 @@
 #include <Watchy.h> //include the Watchy library
 #include <DIN_1451_Engschrift_Regular64pt7b.h>
-#include <DIN_1451_Engschrift_Regular14pt7b.h>
+#include <DIN_1451_Engschrift_Regular12pt7b.h>
 				
 
 class WatchFace : public Watchy { //inherit and extend Watchy class
@@ -24,7 +24,7 @@ class WatchFace : public Watchy { //inherit and extend Watchy class
       //draw hours
       textstring = currentTime.Hour;
       display.getTextBounds(textstring, 0, 0, &x1, &y1, &w, &h);
-      display.setCursor(184-w, 100-5-h);
+      display.setCursor(184-w, 100-5);
       display.print(textstring);
       
       //draw minutes
@@ -35,53 +35,54 @@ class WatchFace : public Watchy { //inherit and extend Watchy class
       }
       textstring += currentTime.Minute;
       display.getTextBounds(textstring, 0, 0, &x1, &y1, &w, &h);
-      display.setCursor(184-w, 100+5);
+      display.setCursor(183-w, 100+3+h);
       display.print(textstring);
 
       // draw battery
       display.fillRoundRect(16,16,34,12,4,light ? GxEPD_BLACK : GxEPD_WHITE);
       display.fillRoundRect(49,20,3,4,2,light ? GxEPD_BLACK : GxEPD_WHITE);
-      display.fillRoundRect(17,17,32,10,3,light ? GxEPD_WHITE : GxEPD_BLACK);
-      float batt = (getBatteryVoltage()-3.8)/0.45;
+      display.fillRoundRect(18,18,30,8,3,light ? GxEPD_WHITE : GxEPD_BLACK);
+      float batt = (getBatteryVoltage()-3.8)/0.40;
       if (batt > 0) {
-        display.fillRoundRect(18,18,30*batt,8,2,light ? GxEPD_BLACK : GxEPD_WHITE);
-												
-								// draw steps
-												display.setFont(&DIN_1451_Engschrift_Regular14pt7b);
-							textstring = sensor.getCounter();
-							textstring += " steps";
+       display.fillRoundRect(20,20,26*batt,4,2,light ? GxEPD_BLACK : GxEPD_WHITE);
+      }									
+
+      display.setFont(&DIN_1451_Engschrift_Regular12pt7b);
+      lasty = 200 - 16;
+
+      //draw steps
+      textstring = sensor.getCounter();
+      textstring += " steps";
       display.getTextBounds(textstring, 0, 0, &x1, &y1, &w, &h);
-							display.setTextColor(light ? GxEPD_WHITE : GxEPD_BLACK);
-							display.fillRoundRect(16,16,w+4,h+4,2,light ? GxEPD_BLACK : GxEPD_WHITE);
-      display.setCursor(18, 200-18-h);
+      display.fillRoundRect(16,lasty-h-2,w + 7,h+4,2,light ? GxEPD_BLACK : GxEPD_WHITE);
+      display.setCursor(19, lasty-3);
+      display.setTextColor(light ? GxEPD_WHITE : GxEPD_BLACK);
       display.print(textstring);
-							lasty = 200-16-h-4;
-												
-								// draw year
-							 display.setTextColor(light ? GxEPD_BLACK : GxEPD_WHITE);
-							textstring = currentTime.Year + 1970;
+      display.setTextColor(light ? GxEPD_BLACK : GxEPD_WHITE);
+      lasty += -8-h;
+
+      // draw year
+      textstring = currentTime.Year + 1970;
       display.getTextBounds(textstring, 0, 0, &x1, &y1, &w, &h);
-      display.setCursor(16, lasty - 6 - h);
+      display.setCursor(16, lasty);
       display.print(textstring);
-							lasty += -6-h;
-												
-							// draw date
-							textstring = monthShortStr(currentTime.Month);
-							textstring += " ";
-							textstring += currentTime.Day;
+      lasty += -20;
+
+      // draw date
+      textstring = monthShortStr(currentTime.Month);
+      textstring += " ";
+      textstring += currentTime.Day;
       display.getTextBounds(textstring, 0, 0, &x1, &y1, &w, &h);
-      display.setCursor(16, lasty - 6 - h);
+      display.setCursor(16, lasty);
       display.print(textstring);
-							lasty += -6-h;
-												
-							// draw day
-							textstring = dayStr(currentTime.Wday);
+      lasty += -20;
+      
+      // draw day
+      textstring = dayStr(currentTime.Wday);
       display.getTextBounds(textstring, 0, 0, &x1, &y1, &w, &h);
-      display.setCursor(16, lasty - 6 - h);
+      display.setCursor(16, lasty);
       display.print(textstring);
-							lasty += -6-h;
-								
-      }
+
     }
 };
 
